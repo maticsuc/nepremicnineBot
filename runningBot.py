bd43c1a6c1f7f65963db13550594c0397c38f1d6
Matic <matic687@gmail.com>
2021-08-26 11:06:10 +0200

    Fixed the running bot

--- runningBot.py
+++ runningBot.py
@@ -6,6 +6,8 @@ URL = "https://www.nepremicnine.net/oglasi-oddaja/ljubljana-mesto/stanovanje/"
 
 json_file = "data/number_of_appartments.json"
 
+interval = 60
+
 bot = WebScraper(URL)
 
 nAppartments_temp = bot.nAppartments
@@ -17,16 +19,16 @@ f.write(json_string)
 f.close()
 
 while True:
-    if bot.getNappartments() != nAppartments_temp:
+    if nAppartments_temp != bot.getNappartments():
+        nAppartments_temp = bot.nAppartments
         current_time = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
-        f = open(json_file, "r", encoding='utf-8')
-        json_string = f.read()
-        data = json.loads(json_string)
-        data[current_time] = bot.nAppartments
+        f = open(json_file, "r")
+        data = json.loads(f.read())
+        data[current_time] = nAppartments_temp
         json_string = json.dumps(data, ensure_ascii=False, indent=4)
-        f = open(json_file, "a", encoding='utf-8')
+        f = open(json_file, "w", encoding='utf-8')
         f.write(json_string)
         f.close()
-        nAppartments_temp = bot.nAppartments
+        print("Updated data!")
 
-    time.sleep(60)
+    time.sleep(interval)
